d0c8e82484af711b39520c61d934638d2c4f24b6
sheepman39 <51085651+sheepman39@users.noreply.github.com>
2020-08-25 14:27:49 -0500

    Create art.py

---
+++ art.py
@@ -0,0 +1,42 @@
+import turtle as trtl
+
+painter = trtl.Turtle()
+x = 20
+repeat = 0
+hexSize = 30
+painter.turtlesize(3)
+painter.pensize(5)
+painter.pencolor('#48f542')
+colorArray = ['#ed0c1f','#48f542','#1dbabf','#c712ac','#f5ad14','#ed0c1f']
+
+#Creates the square with dots
+painter.right(45)
+while repeat < 4:
+  painter.forward(100)
+  painter.right(90)
+  painter.dot(x)
+  repeat = repeat + 1
+painter.setheading(0)
+#Creates the circles
+repeat = 0
+while repeat < len(colorArray):
+  painter.circle(repeat*10+10)
+  painter.pencolor(colorArray[repeat])
+  repeat = repeat +1
+
+painter.penup()
+painter.goto(-150,50)
+painter.pendown()
+painter.pencolor('#0ced93')
+
+#Creates the hexagon
+repeat = 0
+while repeat < 6:
+  painter.forward(hexSize)
+  painter.right(60)
+  painter.stamp()
+  repeat = repeat + 1
+
+wn = trtl.Screen()
+wn.mainloop()
+

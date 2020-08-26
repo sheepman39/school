92bc28c75763c238812ebc7c71b046e5c4632096
sheepman39 <51085651+sheepman39@users.noreply.github.com>
2020-08-26 13:08:23 -0500

    Update shapes.py

--- shapes.py
+++ shapes.py
@@ -7,13 +7,24 @@ painter.pensize(5)
 painter.pencolor('black')
 def shape_creator(sides,length):
   repeat = 0
+  
   while repeat < sides:
     painter.right(360/sides)
     painter.forward(length)
     repeat = repeat + 1
-
+    
+def moveto(x,y):
+  painter.penup()
+  painter.goto(x,y)
+  painter.pendown()
+  
+  
 shape_creator(4,100)
-painter.penup()
-painter.goto(50,0)
-painter.pendown()
+moveto(70,0)
 shape_creator(8,60)
+moveto(-100,100)
+shape_creator(5,80)
+
+
+wn = trtl.Screen()
+wn.mainloop()

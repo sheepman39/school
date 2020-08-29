bd93ac1b96eeddeef6281237c9d500209d13fa96
sheepman39 <jsalata90@gmail.com>
2020-08-29 14:43:24 -0500

    Updated homework 7/31

--- lesson16.py
+++ lesson16.py
@@ -5,32 +5,36 @@ import turtle as trtl
 painter = trtl.Turtle()
 painter.speed(10)
 painter.pensize(5)
+
+# this is a function I made so it was a bit quicker 
+# to move the turtle around and didn't copy and paste
+# the same three lines
 def moveto(x,y):
   painter.penup()
   painter.goto(x,y)
   painter.pendown()
  
 # add code here for a circle
-painter.circle(100)
+painter.circle(50)
 
 # move the turtle to another part of the screen
-moveto(-150,-150)
+moveto(-100,-100)
 
 # add code here for an arc
-painter.circle(100,274)
+painter.circle(60,274)
 
 # move the turtle to another part of the screen
-moveto(150, 150)
+moveto(75,100)
 
 # add code here for an arc that is greater than 90 degrees and has 5 steps
-painter.circle(100, 290, 5)
+painter.circle(50, 290, 5)
 
 # move the turtle to another part of the screen
-moveto(-150,150)
+moveto(-100,100)
 
 # add code here to create a polygon of your choice using the circle method
 painter.setheading(180)
-painter.circle(100,360,8)
+painter.circle(75,360,8)
 
 # create screen object and make it persist
 wn = trtl.Screen()

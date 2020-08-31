4e0e6dd5e2fb848bbed7b8cf406c148961c6c2ec
sheepman39 <51085651+sheepman39@users.noreply.github.com>
2020-08-31 13:11:06 -0500

    Update lesson16.py

--- lesson16.py
+++ lesson16.py
@@ -14,23 +14,23 @@ def moveto(x,y):
 painter.circle(100)
 
 # move the turtle to another part of the screen
-moveto(-150,-150)
+moveto(-100,-100)
 
 # add code here for an arc
-painter.circle(100,274)
+painter.circle(75,274)
 
 # move the turtle to another part of the screen
-moveto(150, 150)
+moveto(25, -100)
 
 # add code here for an arc that is greater than 90 degrees and has 5 steps
-painter.circle(100, 290, 5)
+painter.circle(75, 290, 5)
 
 # move the turtle to another part of the screen
-moveto(-150,150)
+moveto(-100,100)
 
 # add code here to create a polygon of your choice using the circle method
-painter.setheading(180)
-painter.circle(100,360,8)
+painter.setheading(90)
+painter.circle(45,360,8)
 
 # create screen object and make it persist
 wn = trtl.Screen()

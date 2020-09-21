62ad87ae14e373391da692b2cf24b3c30d943fef
sheepman39 <51085651+sheepman39@users.noreply.github.com>
2020-09-21 14:32:11 -0500

    Create 1.1.8_grade_checker.py

---
+++ 1.1.8_grade_checker.py
@@ -0,0 +1,25 @@
+#   a118_grades.py
+#   This code is incomplete. 
+
+my_courses = ("English","CS","Band")
+check_grades = True
+while (check_grades):
+  
+  for course in my_courses:
+    print("Enter your points for " + course)
+    a = int(input("Points -> "))
+    if (a >= 90):
+      print("A")
+    elif (a >= 80):
+      print("B")
+    elif (a >= 70):
+      print("C")
+    elif (a >= 60):
+      print("D")
+    else:
+      print("F")
+    redo = input("Do you need to re-do these grades? (y/n)")
+    if (redo == "y"):
+      check_grades = True
+    else:
+      check_grades = False

17799cab9a6cf8d176e18f588ccd06b6cf416dc6
sheepman39 <51085651+sheepman39@users.noreply.github.com>
2020-09-22 14:39:37 -0500

    Update 1.1.8_project.py

--- 1.1.8_project.py
+++ 1.1.8_project.py
@@ -6,7 +6,6 @@ import turtle as trtl
 # create two empty lists of turtles, adding to them later
 horiz_turtles = []
 vert_turtles = []
-
 # use interesting shapes and colors
 turtle_shapes = ["arrow", "turtle", "circle", "square", "triangle", "classic"]
 horiz_colors = ["red", "blue", "green", "orange", "purple", "gold"]
@@ -14,31 +13,39 @@ vert_colors = ["darkred", "darkblue", "lime", "salmon", "indigo", "brown"]
 
 tloc = 50
 for s in turtle_shapes:
-
+  #Adds a new turtle to the horiz_turtles list
   ht = trtl.Turtle(shape=s)
   horiz_turtles.append(ht)
   ht.penup()
   new_color = horiz_colors.pop()
   ht.fillcolor(new_color)
-  ht.goto(-350, tloc)
+  ht.goto(-150, tloc)
   ht.setheading(0)
-
+  #Adds a new turtle to the vert_turtles list
   vt = trtl.Turtle(shape=s)
   vert_turtles.append(vt)
   vt.penup()
   new_color = vert_colors.pop()
   vt.fillcolor(new_color)
-  vt.goto( -tloc, 350)
+  vt.goto( -tloc, 150)
   vt.setheading(270)
   
+  #Changes the location of the turtles
   tloc += 50
 
 # TODO: move turtles across and down screen, stopping for collisions
-"""
-steps = 0
-while steps < 50:
-	steps = steps + 1
-"""
 
+  steps = 0
+  while steps < 50: 
+    steps = steps + 1
+    for v in vert_turtles:
+      for h in horiz_turtles:
+        ht.forward(3)
+        vt.forward(3)
+        if(abs(ht.xcor()-vt.xcor()) < 20 and abs(ht.ycor()-vt.ycor()) < 20):
+          vt.fillcolor("gray")
+          vert_turtles.remove(v)
+          ht.fillcolor("gray")
+          horiz_turtles.remove(h) 
 wn = trtl.Screen()
 wn.mainloop()

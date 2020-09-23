f1e8ea1d8cac3d0474d317de3a14b030c5a5bc21
sheepman39 <51085651+sheepman39@users.noreply.github.com>
2020-09-23 14:27:53 -0500

    Update 1.1.8_project.py

--- 1.1.8_project.py
+++ 1.1.8_project.py
@@ -7,7 +7,7 @@ import turtle as trtl
 horiz_turtles = []
 vert_turtles = []
 # use interesting shapes and colors
-turtle_shapes = ["arrow", "turtle", "circle", "square", "triangle", "classic"]
+turtle_shapes = ["arrow", "turtle", "circle", "triangle", "classic"]
 horiz_colors = ["red", "blue", "green", "orange", "purple", "gold"]
 vert_colors = ["darkred", "darkblue", "lime", "salmon", "indigo", "brown"]
 
@@ -19,7 +19,7 @@ for s in turtle_shapes:
   ht.penup()
   new_color = horiz_colors.pop()
   ht.fillcolor(new_color)
-  ht.goto(-150, tloc)
+  ht.goto(-100, tloc)
   ht.setheading(0)
   #Adds a new turtle to the vert_turtles list
   vt = trtl.Turtle(shape=s)
@@ -27,25 +27,47 @@ for s in turtle_shapes:
   vt.penup()
   new_color = vert_colors.pop()
   vt.fillcolor(new_color)
-  vt.goto( -tloc, 150)
+  vt.goto( -tloc, 100)
   vt.setheading(270)
   
   #Changes the location of the turtles
-  tloc += 50
+  tloc += 25
 
 # TODO: move turtles across and down screen, stopping for collisions
 
   steps = 0
   while steps < 50: 
     steps = steps + 1
+    speed = 1
     for v in vert_turtles:
       for h in horiz_turtles:
+        if(speed > 1 and speed < 5):
+          speed += 1
+        else:
+          speed -+ 1
+        ht.speed(speed)
+        vt.speed(speed)
         ht.forward(3)
         vt.forward(3)
         if(abs(ht.xcor()-vt.xcor()) < 20 and abs(ht.ycor()-vt.ycor()) < 20):
-          vt.fillcolor("gray")
-          vert_turtles.remove(v)
+          ht_shape = ht.shape()
+          vt_shape = vt.shape()
+          vt.fillcolor("#a2d145")
+          ht.fillcolor("#a2d145")
+          vt.shape("square")
+          ht.shape("square")
+          #vert_turtles.remove(v)
+          #horiz_turtles.remove(h)
+          vt.forward(-30)
+          ht.forward(-45)
+        #else: 
+          #vt.shape(vt_shape)
+          #ht.shape(ht_shape)
+        if(abs(ht.xcor()) > 180 or abs(ht.ycor() > 180)):
           ht.fillcolor("gray")
           horiz_turtles.remove(h) 
+        if(abs(vt.xcor()) > 180 or abs(vt.ycor() > 180)):
+          vt.fillcolor("gray")
+          vert_turtles.remove(v)
 wn = trtl.Screen()
 wn.mainloop()

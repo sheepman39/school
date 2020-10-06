7719f68828a74f5bb71df019de4aabd06351c83e
sheepman40 <67875244+sheepman40@users.noreply.github.com>
2020-10-06 13:53:18 -0500

    Create 1.2.1_project1.py

---
+++ 1.2.1_project1.py
@@ -0,0 +1,14 @@
+# a121_catch_a_turtle.py
+#-----import statements-----
+
+
+#-----game configuration----
+
+
+#-----initialize turtle-----
+
+
+#-----game functions--------
+
+
+#-----events----------------

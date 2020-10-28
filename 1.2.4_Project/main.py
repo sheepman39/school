54e3723d9f5974c21f0a1ac306b17855e6c80745
sheepman39 <jsalata90@gmail.com>
2020-10-28 19:41:15 +0000

    Project through number 10. Works

--- 1.2.4_Project/main.py
+++ 1.2.4_Project/main.py
@@ -7,18 +7,45 @@ wn.setup(width=1.0, height=1.0)
 
 # config variables
 num_walls = 25
-distance = 20
-wall_color = "#4a1a9c"
+distance = 15
+wall_color = "#007502"
 
 # maze drawer setup
 maze_painter = trtl.Turtle()
 maze_painter.pensize(5)
 maze_painter.color(wall_color)
+maze_painter.ht()
+
+# maze runner
+maze_run = trtl.Turtle()
 
 # maze creator
 for i in range(num_walls):
 
-  maze_painter.left(90)
-  maze_painter.forward(20+i*distance)
+  # Controls doors and walls
+  # ignores the first 6 walls drawn
+  if(i>=6):
+    
+    #draws the windows
+    maze_painter.left(90)
+    maze_painter.forward(10)
+    maze_painter.penup()
+    maze_painter.forward(distance)
+    maze_painter.pendown()
+    
+    #Draws the barriers
+    maze_painter.forward(40)
+    maze_painter.left(90)
+    maze_painter.forward(distance*2)
+    maze_painter.back(distance*2)
+    maze_painter.right(90)
+    maze_painter.forward(-40+i*distance)
+
+  else:
+    
+    # draws the first 6 walls
+    maze_painter.left(90)
+    maze_painter.forward(20+i*distance)
+  
 
 wn.mainloop()

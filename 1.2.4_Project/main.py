8b271055112bd61e7716be93f486f2e211ed2abc
sheepman39 <jsalata90@gmail.com>
2020-10-28 18:43:01 +0000

    Just the base of the code. Nothing done yet

---
+++ 1.2.4_Project/main.py
@@ -0,0 +1,15 @@
+# 1.2.4_Project with the mazes
+import turtle as trtl 
+
+# Screen setup
+wn = trtl.screen()
+wn.setup(width=1.0, height=1.0)
+
+# config variables
+num_walls = 5
+distance = 20
+wall_color = "#4a1a9c"
+
+# maze creator
+
+wn.mainloop()

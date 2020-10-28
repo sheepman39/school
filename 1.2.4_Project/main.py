e4df944ca1ab87235a2903dc2816b739a43d96d4
sheepman39 <jsalata90@gmail.com>
2020-10-28 19:07:15 +0000

    Up to step 2

--- 1.2.4_Project/main.py
+++ 1.2.4_Project/main.py
@@ -2,14 +2,23 @@
 import turtle as trtl 
 
 # Screen setup
-wn = trtl.screen()
+wn = trtl.Screen()
 wn.setup(width=1.0, height=1.0)
 
 # config variables
-num_walls = 5
+num_walls = 25
 distance = 20
 wall_color = "#4a1a9c"
 
+# maze drawer setup
+maze_painter = trtl.Turtle()
+maze_painter.pensize(5)
+maze_painter.color(wall_color)
+
 # maze creator
+for i in range(num_walls):
+
+  maze_painter.left(90)
+  maze_painter.forward(20+i*distance)
 
 wn.mainloop()

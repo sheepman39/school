df52c88081c21bcf61b09920adfc1adfc2b7e793
sheepman39 <jsalata90@gmail.com>
2020-10-29 19:35:55 +0000

    WIP Very broken

--- 1.2.4_Project/main.py
+++ 1.2.4_Project/main.py
@@ -1,5 +1,6 @@
 # 1.2.4_Project with the mazes
 import turtle as trtl 
+import random as rand
 
 # Screen setup
 wn = trtl.Screen()
@@ -24,28 +25,70 @@ for i in range(num_walls):
 
   # Controls doors and walls
   # ignores the first 6 walls drawn
+  wall_len = 20+i*distance
+
   if(i>=6):
     
-    #draws the windows
-    maze_painter.left(90)
-    maze_painter.forward(10)
-    maze_painter.penup()
-    maze_painter.forward(distance)
-    maze_painter.pendown()
+    door = rand.randint(distance*2, (wall_len - distance*2))
+    barrier = rand.randint(distance*2, (wall_len - distance*2))
+    while abs(door - barrier) < distance:
+      door = rand.randint(distance*2, (wall_len - distance*2))
     
-    #Draws the barriers
-    maze_painter.forward(40)
-    maze_painter.left(90)
-    maze_painter.forward(distance*2)
-    maze_painter.back(distance*2)
-    maze_painter.right(90)
-    maze_painter.forward(-40+i*distance)
+    if(door < barrier):
+      maze_painter.left(90)
+      maze_painter.forward(door)
+      maze_painter.penup()
+      maze_painter.forward(distance)
+      maze_painter.pendown()
+      maze_painter.forward(abs(door-barrier))
+      maze_painter.left(90)
+      maze_painter.forward(distance*2)
+      maze_painter.back(distance*2)
+      maze_painter.right(90)
+      maze_painter.forward(abs(wall_len-(door+barrier)))
+    else:
+      maze_painter.left(90)
+      maze_painter.forward(abs(door-barrier))
+      maze_painter.left(90)
+      maze_painter.forward(distance*2)
+      maze_painter.back(distance*2)
+      maze_painter.right(90)
 
+      maze_painter.forward(door)
+      maze_painter.penup()
+      maze_painter.forward(distance)
+      maze_painter.pendown()
+
+      maze_painter.forward(abs(wall_len-(door+barrier)))
+      '''
+      #draws the windows
+      maze_painter.left(90)
+      maze_painter.forward(door)
+      maze_painter.penup()
+      maze_painter.forward(distance)
+      maze_painter.pendown()
+      
+      
+      #Draws the barriers
+      maze_painter.forward(40)
+      maze_painter.left(90)
+      maze_painter.forward(distance*2)
+      maze_painter.back(distance*2)
+      maze_painter.right(90)
+      maze_painter.forward(-40+i*distance)
+'''
   else:
     
     # draws the first 6 walls
     maze_painter.left(90)
     maze_painter.forward(20+i*distance)
-  
+
 
 wn.mainloop()
+
+
+
+
+# snek easter egg
+# hello there
+# \/\/\/\/\/\/\/\/\/\/\/\/\/\/[}=>

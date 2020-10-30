ed7246bb8fa59280de639f318f2d07481f74d118
sheepman39 <jsalata90@gmail.com>
2020-10-29 21:39:29 -0500

    Very broken

--- 1.2.4_Project/main.py
+++ 1.2.4_Project/main.py
@@ -20,46 +20,76 @@ maze_painter.ht()
 # maze runner
 maze_run = trtl.Turtle()
 
+def coordinate_checker(original,turtle):
+  if(abs(original-turtle.xcor()) == 0):
+    return int(turtle.ycor())
+  elif(abs(original-turtle.ycor()) == 0):
+    return int(turtle.xcor())
+  else: 
+    return
 # maze creator
 for i in range(num_walls):
 
   # Controls doors and walls
   # ignores the first 6 walls drawn
-  wall_len = 20+i*distance
+  wall_len = 30+i*distance
 
   if(i>=6):
-    
+    print("i: " + str(i))
     door = rand.randint(distance*2, (wall_len - distance*2))
     barrier = rand.randint(distance*2, (wall_len - distance*2))
     while abs(door - barrier) < distance:
       door = rand.randint(distance*2, (wall_len - distance*2))
     
+    change_cor = []
     if(door < barrier):
+
       maze_painter.left(90)
       maze_painter.forward(door)
       maze_painter.penup()
-      maze_painter.forward(distance)
+      maze_painter.forward(distance*2)
       maze_painter.pendown()
-      maze_painter.forward(abs(door-barrier))
+
+      maze_painter.forward(abs((door+distance*2) - barrier))
       maze_painter.left(90)
       maze_painter.forward(distance*2)
       maze_painter.back(distance*2)
       maze_painter.right(90)
-      maze_painter.forward(abs(wall_len-(door+barrier)))
+      maze_painter.forward(abs(wall_len-(barrier + distance*2)))
+      print("===first=loop===")
+      print("door: " + str(door))
+      print("barrier: " + str(barrier))
+      print("Door - Barrier" + str(abs(door-barrier)))
+      print("Door + Barrier: " + str(door + barrier))
+      print("wall_len: " + str(wall_len))
+      print("wall_len left: " + str(abs(wall_len-(barrier + distance*2))))
+      print("Change in coordiantes: " + str(abs(change_cor - coordinate_checker(change_cor,maze_painter))))
+      print("===================")
+
     else:
+
       maze_painter.left(90)
-      maze_painter.forward(abs(door-barrier))
+      maze_painter.forward(barrier)
       maze_painter.left(90)
       maze_painter.forward(distance*2)
       maze_painter.back(distance*2)
       maze_painter.right(90)
 
-      maze_painter.forward(door)
+      maze_painter.forward(abs(door-barrier))
       maze_painter.penup()
-      maze_painter.forward(distance)
+      maze_painter.forward(distance*2)
       maze_painter.pendown()
-
-      maze_painter.forward(abs(wall_len-(door+barrier)))
+      maze_painter.forward(abs(wall_len - (door + distance*2)))
+      #maze_painter.forward(abs(wall_len-(door+(barrier+distance*2))))
+      print("===second=loop===")
+      print("door: " + str(door))
+      print("barrier: " + str(barrier))
+      print("Door - Barrier" + str(abs(door-barrier)))
+      print("Door + Barrier: " + str(door + barrier))
+      print("wall_len: " + str(wall_len))
+      print("wall_len left: " + str(abs(wall_len -(door + distance*2))))
+      print("Change in coordiantes: " + str(abs(change_cor - coordinate_checker(change_cor,maze_painter))))
+      print("===================")
       '''
       #draws the windows
       maze_painter.left(90)

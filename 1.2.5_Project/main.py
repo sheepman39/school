d5798fcf303dab478dca1b6d4752345cc726954d
sheepman39 <jsalata90@gmail.com>
2020-11-05 20:35:39 +0000

    Our custom game project on separate branch

---
+++ 1.2.5_Project/main.py
@@ -0,0 +1,22 @@
+#    a.113---main.py---Shall we play a game?
+
+#import statements
+import turtle as trtl 
+import random as rand
+import functools
+
+# idea: falling raindrops that click
+# you get a score
+# steal code from 1.2.2 with the leaderboard
+# I have some javascript code we can convert for this
+# timer?
+# Idea: add different color raindrops that you
+# don't click on
+# TODO: make raindrop fall
+# TODO: make multiple fall + random location
+# TODO: make them clickable
+# TODO: make them disappear and count when clicked
+# TODO: add timer
+# TODO: implement leaderboard code from previous projects
+# TODO: Add different colored raindrops
+# TODO: add easter eggs 

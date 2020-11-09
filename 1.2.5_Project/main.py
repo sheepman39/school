f5edcf27a19b7a7a6b8d9cbe1b6a77ae52af7ee9
sheepman39 <jsalata90@gmail.com>
2020-11-09 14:47:33 +0000

    unkown

--- 1.2.5_Project/main.py
+++ 1.2.5_Project/main.py
@@ -11,21 +11,23 @@ import functools
 # I have some javascript code we can convert for this
 # timer?
 # Idea: add different color raindrops that you don't click on
-#https://www.khanacademy.org/computer-programming/snow-fall/6097804743835648
+#https://wwwkhanacademy.org/computer-programming/snow-fall/6097804743835648
 
 # === variables ===
 raindrops = []
+wn = trtl.Screen()
+wn.setup(width=1.0, height=1.0)
 
 for i in range(10):
   tmp_turtle = trtl.Turtle()
   tmp_turtle.penup()
-  tmp_turtle.size(rand.randint(0.5,3))
-  tmp_turtle.speed(rand.randint(0.5,5))
-  tmp_turtle.goto(rand.randint(-100,100),rand.randint(-100,100))
+  tmp_turtle.turtlesize(rand.uniform(0.5,3))
+  tmp_turtle.speed(rand.uniform(0.5,5))
+  tmp_turtle.goto(rand.randint(-150,150),rand.randint(-100,100))
     
 
-for i in range(len(raindrop_pos)):
-  
+for i in range(len(raindrops)):
+  raindrops[i].goto(raindrops[i].xPos,0)
 # TODO: make raindrop fall
 '''
 var xPositions = [200];
@@ -65,13 +67,78 @@ draw = function() {
 # TODO: add timer
 
 #Timer turtle
+'''
 counter =  trtl.Turtle()
 counter.penup()
 counter.hideturtle()
 counter.goto(-150,-150)
 
+global timer
+global counter_interval
+global timer_up
+
+if(timer_up == True):
+    counter.clear()
+    counter.write("Timer: " + str(timer), font=font_setup)
+    counter.getscreen().ontimer(countdown, counter_interval) 
+    dot_color = "#460a8f"
+    dot_size = 5
+    dot_shape = "circle"
+    score = 0
+    timer = 20
+    font_setup = ("Arial", 20, "normal")
+    counter_interval = 1000  
+    timer_up = False
+    
+  else:
+    spot_clicked(x,y)
+  
+#Timer code
+def countdown():
+  
+  global timer, timer_up
+  counter.clear()
+  if (timer <= 0):
+  
+    counter.write("Time's Up", font=font_setup)
+    timer_up = True
+    manage_leaderboard()
+    dot.goto(0,0)
+    dot.turtlesize(5)
+    print("Click on the turtle to play again!")
+'''
+
 # TODO: implement leaderboard code from previous projects
+'''
+# manages the leaderboard for top 5 scorers
+def manage_leaderboard():
+
+    global leader_scores_list
+    global leader_names_list
+    global score
+    global dot
+
+    # load all the leaderboard records into the lists
+    lb.load_leaderboard(leaderboard_file_name, leader_names_list,
+                        leader_scores_list)
+
+    # TODO
+    if (len(leader_scores_list) < 5 or score > leader_scores_list[4]):
+        lb.update_leaderboard(leaderboard_file_name, leader_names_list,
+                              leader_scores_list, player_name, score)
+        lb.draw_leaderboard(leader_names_list, leader_scores_list, True, dot,
+                            score)
+
+    else:
+        lb.draw_leaderboard(leader_names_list, leader_scores_list, False, dot,
+                            score)
+'''
+
 # TODO: Add different colored raindrops
+
+
 # TODO: add easter eggs 
 #/\//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/[}--<
-#where code?
+wn.listen()
+wn.mainloop()
+

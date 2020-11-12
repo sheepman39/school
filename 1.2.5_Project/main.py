f1d5ccd63b18fd5678f3a1b8b2e2dc8689efee28
sheepman39 <jsalata90@gmail.com>
2020-11-12 20:46:42 +0000

    Updates

--- 1.2.5_Project/main.py
+++ 1.2.5_Project/main.py
@@ -6,20 +6,18 @@ import random as rand
 import functools
 import leaderboard as lb
 
-# idea: falling trashtrash that click
-# you get a score
-# steal code from 1.2.2 with the leaderboard
-# I have some javascript code we can convert for this
-# timer?
-# Idea: add different color trash that you don't click on
-#https://wwwkhanacademy.org/computer-programming/snow-fall/6097804743835648
-
 # === variables ===
+point = 0
+rounds = 0
+lives = 3
+
 trash = []
 trash_img = ["./1.2.5_Project/bad_apple.gif","./1.2.5_Project/banana.gif","./1.2.5_Project/garbage-bag.gif","./1.2.5_Project/paper_ball.gif"]
 trashcan = "./1.2.5_Project/trashcan.gif"
 tipped_trash = "./1.2.5_Project/tippedovercan.gif"
 font_setup = ("Arial", 20, "normal")
+
+# screen setup
 wn = trtl.Screen()
 wn.setup(width=1.0, height=1.0)
 
@@ -29,18 +27,13 @@ for i in trash_img:
 
 wn.addshape(trashcan) 
 wn.addshape(tipped_trash)
-point = 0
-rounds = 0
-lives = 3
+
 
 # === leaderboard variables ===
 leaderboard_file_name = "./1.2.5_Project/1.2.5_Leaderboard.txt"
 leader_names_list = []
 leader_scores_list = []
-try:
-  player_name = wn.textinput("Greetings Player...","I'm gonna need your name player. Player isn' exactly a name...:")
-except:
-  player_name = "Unknown"
+
 
 # === bucket turtle ===
 bucket = trtl.Turtle()
@@ -56,12 +49,16 @@ scoreboard.penup()
 scoreboard.goto(-350,-200)
 scoreboard.ht()
 
-#easter egg number 1
-#           {YEEEEEEE HAAAAAAWWWWWWW!!!!!}
+# easter egg number 1
+#           {Hi Mr. Joyce}
 #        o /
 #       /|\
 #       / \
-#------------------
+# === inital setup ===
+try:
+  player_name = wn.textinput("Greetings Player...","I'm gonna need your name player. Player isn' exactly a name...:")
+except:
+  player_name = "Unknown"
 # === functions ===
 # manages the leaderboard for top 5 scorers
 def manage_leaderboard():
@@ -74,7 +71,7 @@ def manage_leaderboard():
   # load all the leaderboard records into the lists
   lb.load_leaderboard(leaderboard_file_name, leader_names_list,leader_scores_list)
 
-  # TODO
+  # controls writing to the leaderboard
   if (len(leader_scores_list) < 5 or point > leader_scores_list[4]):
       lb.update_leaderboard(leaderboard_file_name,leader_names_list,leader_scores_list, player_name, point)
       lb.draw_leaderboard(leader_names_list, leader_scores_list, True, bucket,point)
@@ -85,8 +82,12 @@ def manage_leaderboard():
 # creates the items of trash
 def trash_creator(x):
   global trash
+
+  # creates x amount of turtles
   for i in range(x):
     tmp_turtle = trtl.Turtle()
+
+    # sets the shape to a random image
     tmp_turtle.shape(trash_img[rand.randint(0,len(trash_img)-1)])
     tmp_turtle.penup()
     tmp_turtle.turtlesize(rand.uniform(0.5,3))
@@ -103,65 +104,79 @@ def move(direction):
   elif(str(direction) == 'right'):
     bucket.goto(bucket.xcor()+14,bucket.ycor())
 
-
-
-# tells the turtles to fall
+# tells the turtles to fall and control mosts of the game
 def game_start():
   global point
   global lives 
   global rounds
   global lives
+
+  # repeats this for each peice of trash
   for i in range(len(trash)):
 
+    # if lives are 0, game is over and break out of loop
     if(lives <= 0):
-      print("Game Over")
       break
-      
-    scoreboard.clear()
-    scoreboard.write("Score: " + str(point) + "\nRound " + str(rounds) + "\nLives: "  + str(lives),font=font_setup)
 
+    # updates the scoreboard on the left
+    scoreboard.clear()
+    scoreboard.write("Score: " + str(point) + "\nRound: " + str(rounds) + "\nLives: "  + str(lives),font=font_setup)
+
+    # repeats this for trash reamining
     while(trash[i].isvisible()):
 
-      # adds functionality to move trash can
+      # Listnes for the arrow or a and d keys to move
       wn.onkeypress(functools.partial(move,"left"),"a")
       wn.onkeypress(functools.partial(move,"left"), "Left")
       wn.onkeypress(functools.partial(move,"right"),"d")
       wn.onkeypress(functools.partial(move,"right"),"Right")
       wn.listen()
 
-
+      # collision checker
       if(abs(trash[i].xcor()-bucket.xcor()) < 25 and trash[i].ycor() <= -200):
         
         trash[i].ht()
         point += 1
 
+      # if the trash is below this point, lose a life
       elif(trash[i].ycor() <= -320):
       
         trash[i].ht()
         lives -= 1
 
-
-
+      # moves the trash down
       trash[i].goto(trash[i].xcor(),trash[i].ycor()- float(trash[i].speed()))
-  
 
-# summons 10 test dummies
+#easter egg number 2
+#         ---
+#        /   
+
+# While you still have lives,
+# run the game
 while(lives>0):
   
-  trash_creator(10)
+  # summons more trash per round
+  trash_creator(10 + rounds*2)
   rounds += 1
 
+  # bonus life if you live for 5 rounds
   if(rounds % 5 == 0):
     lives += 1
   
+  # starts the ame
   game_start()
 
+# if the lives are less than zero
 if(lives <= 0):
   
+  # hide any extra trash
   for i in trash:
     i.ht()
 
+  # manage the leaderboard
   manage_leaderboard()
+
+  # displays fallen trash bin
   bucket.goto(0,-180)
   bucket.st()
   bucket.shape(tipped_trash)
@@ -170,6 +185,7 @@ if(lives <= 0):
 # TODO: implement leaderboard code from previous projects
 # TODO: Add different colored trashtrash
 # TODO: add easter eggs 
+#easter egg 
 #/\//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/[}--<
 wn.listen()
 wn.mainloop()

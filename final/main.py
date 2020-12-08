30a8b159854a80b6ddfeb43b944c7bc8a55c63bf
sheepman39 <jsalata90@gmail.com>
2020-12-08 20:25:26 +0000

    Started the brainstorm process

---
+++ final/main.py
@@ -0,0 +1,18 @@
+# Final project by
+# Tyler <import last name>
+# Thomas Armstrong
+# John Salata
+
+# we are making a dueling game
+# TODO
+# make two turtles that can move using
+# wasd and arrow keys
+# make the turtles able to fire projectiles 
+# make the opposite player able to be hit
+# make a scoreboard for when a player gets hit
+# insert player names
+# change turtle colors
+# fix bugs
+# possible upgrades?
+# make some obstacles to hide behind
+# different blaster models

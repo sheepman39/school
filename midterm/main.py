c68285333e561d95e43d986e3f6024f19ecf122a
sheepman39 <jsalata90@gmail.com>
2020-12-12 13:11:11 -0600

    Fixed Projectiles and many can move at once

--- midterm/main.py
+++ midterm/main.py
@@ -28,6 +28,10 @@ wn.setup(width=1.0, height=1.0)
 speed = 10
 circles = list()
 
+# test turtle
+test1 = trtl.Turtle()
+test1.goto(0,100)
+test1.goto(0,-100)
 
 # player 1
 player1 = trtl.Turtle()
@@ -40,10 +44,11 @@ player1.health = 20
 
 # player 2
 player2 = trtl.Turtle()
-player2.shape("square")
+player2.shape("arrow")
 player2.turtlesize(3)
 player2.color("green")
 player2.penup()
+player2.setheading(180)
 player2.goto(200,0)
 player2.health = 20
 
@@ -52,19 +57,23 @@ class Projectile():
   speed = 10
 
   def __init__(self, player):
+    print(player.heading())
     self.x = player.xcor()
     self.y = player.ycor()
     self.turtle = trtl.Turtle()
     self.turtle.penup()
+    self.turtle.goto(self.x,self.y)
     self.turtle.shape("circle")
     self.turtle.color(player.fillcolor())
     self.direction = player.heading()
-    self.life = 20000
-    circles.append(self)
+    self.turtle.setheading(player.heading())
+    self.life = 50
 
   def move(self,t):
-    self.life, dist = self.life - t, Projectile.speed * t
+
+    self.life, dist = self.life - t, Projectile.speed * t 
     self.x, self.y = self.x + dist * math.cos(self.direction), self.y + dist * math.sin(self.direction)
+    self.turtle.goto(self.x,self.turtle.ycor())
 
 
 # functions
@@ -87,26 +96,30 @@ def move_player(key):
     elif(key == "j" and player2.xcor() > -300):
       player2.forward(-speed)
     elif(key == "i" and player2.ycor() < 200):
-      player2.goto(player2.xcor(),player2.ycor()+speed)
-    elif(key == "k" and player2.ycor() > -200):
       player2.goto(player2.xcor(),player2.ycor()-speed)
+    elif(key == "k" and player2.ycor() > -200):
+      player2.goto(player2.xcor(),player2.ycor()+speed)
 
 def animate():
   for b in circles:
     
-    b.move(1/5)
-    b.draw()
+    Projectile.move(b,1/20)
     if len(circles) > 0:
-        if circles[0].life < 0:
-            circles.pop(0)
-  wn.ontimer(animate, 50)
-    
+      if b.life < 0:
+        circles.pop(0)
+  trtl.update()
+  wn.ontimer(animate, 5)
+  
+def new_projectile(player):
+  c = Projectile(player)
+  circles.append(c)
+
 for k in "wasdilkj":
     
   wn.onkeypress(functools.partial(move_player, k),key = k)
   
-  wn.onkeypress(functools.partial(Projectile,player1),key = 'q')
-  #circles.append(c)
+  wn.onkeypress(functools.partial(new_projectile,player1),key = 'q')
+  wn.onkeypress(functools.partial(new_projectile,player2),key = "o")
   animate()
   wn.listen()
 wn.mainloop()

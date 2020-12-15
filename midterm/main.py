c5d6040ffa43e6875d3ee16fd70d2d030e08223d
sheepman39 <jsalata90@gmail.com>
2020-12-15 20:29:33 +0000

    Made the health. Needs fixing"

--- midterm/main.py
+++ midterm/main.py
@@ -29,8 +29,10 @@ wn.setup(width=1.0, height=1.0)
 wn.bgpic("midterm/background.gif")
 
 # variables
-speed = 20
+speed = 30
+damage = 10
 circles = list()
+font_setup = ("Arial", 20, "bold")
 
 # test turtle
 test1 = trtl.Turtle()
@@ -44,7 +46,15 @@ player1.turtlesize(3)
 player1.color("blue")
 player1.penup()
 player1.goto(-200,0)
-player1.health = 20
+player1.health = 100
+
+# player 1 health
+player1_health = trtl.Turtle()
+player1_health.ht()
+player1_health.penup()
+player1_health.color("blue")
+player1_health.goto(-200,-150)
+player1_health.write("Health: " + str(player1.health),font=font_setup)
 
 # player 2
 player2 = trtl.Turtle()
@@ -54,7 +64,15 @@ player2.color("green")
 player2.penup()
 player2.setheading(180)
 player2.goto(200,0)
-player2.health = 20
+player2.health = 100
+
+# player 2 health
+player2_health = trtl.Turtle()
+player2_health.ht()
+player2_health.penup()
+player2_health.color("green")
+player2_health.goto(200,-150)
+player2_health.write("Health: " + str(player2.health),font=font_setup)
 
 # projectiles
 class Projectile():
@@ -81,11 +99,48 @@ class Projectile():
   # the projectile goes to
   # Only moves on the x-axis
   def move(self,t):
-
+    global circles
     self.life, dist = self.life - t, speed * t 
     self.x, self.y = self.x + dist * math.cos(self.direction), self.y + dist * math.sin(self.direction)
-    self.turtle.goto(self.x,self.turtle.ycor())
+    if self.turtle.fillcolor() == player1.fillcolor():
+      if(abs(self.turtle.xcor()-player2.xcor()) < 20):
+        if(abs(self.turtle.ycor() - player2.ycor()) < 20):
+          self.turtle.ht()
+          self.turtle.clear()
 
+          try:
+            circles.remove(self)
+            player2_health.clear()
+            player2.health -= damage
+            player2_health.write("Health: " + str(player2.health),font=font_setup)
+            print("Hit player 2")
+            return True
+          except:
+            print("error 119")
+            return True
+      else:
+        self.turtle.goto(self.x,self.turtle.ycor())
+
+    elif self.turtle.fillcolor() == player2.fillcolor():
+      if(abs(self.turtle.xcor()-player1.xcor()) < 20):
+        if(abs(self.turtle.ycor() - player1.ycor()) < 20):
+          
+          self.turtle.ht()
+          self.turtle.clear()
+
+          try:
+            circles.remove(self)
+            player1_health.clear()
+            player1.health -= 10
+            player1_health.write("Health: " + str(player1.health),font=font_setup)
+            print("hit player 1")
+            return True
+          except:
+            print("error 136")
+            return True
+      else:
+        self.turtle.goto(self.x,self.turtle.ycor())
+      return False
 
 # functions
 
@@ -93,24 +148,24 @@ class Projectile():
 def move_player(key):
   if(key == "w" or key == "a" or key == "s" or key == "d"):
 
-    if(key == "d" and player1.xcor() < 300):
+    if(key == "d" and abs(player1.xcor()) < 300):
       player1.forward(speed)
-    elif(key == "a" and player1.xcor() > -300):
+    elif(key == "a" and abs(player1.xcor()) < 300):
       player1.forward(-speed)
-    elif(key == "w" and player1.ycor() < 200):
+    elif(key == "w" and abs(player1.ycor()) < 200):
         player1.goto(player1.xcor(),player1.ycor()+speed)
-    elif(key == "s" and player1.ycor() > -200):
+    elif(key == "s" and abs(player1.ycor()) < 200):
       player1.goto(player1.xcor(),player1.ycor()-speed)
 
   elif(key == "i" or key == "k" or key == "l" or key == "j"):
 
-    if(key == "j" and player2.xcor() < 300):
+    if(key == "j" and abs(player2.xcor()) < 300):
       player2.forward(speed)
-    elif(key == "l" and player2.xcor() > -300):
+    elif(key == "l" and abs(player2.xcor()) < 300):
       player2.forward(-speed)
-    elif(key == "k" and player2.ycor() < 200):
+    elif(key == "k" and abs(player2.ycor()) < 200):
       player2.goto(player2.xcor(),player2.ycor()-speed)
-    elif(key == "i" and player2.ycor() > -200):
+    elif(key == "i" and abs(player2.ycor()) < 200):
       player2.goto(player2.xcor(),player2.ycor()+speed)
 
 # this function animates all projectiles at once
@@ -120,15 +175,17 @@ def animate():
   for b in circles:
     
     # they move a certain bit
-    Projectile.move(b,1/20)
+    hit = Projectile.move(b,1/20)
     if len(circles) > 0:
+      
       # if the life is less than 0, remove it
-      if b.life < 0:
-        circles.pop(0)
+      if b.life < 0 and hit == False:
+        b.turtle.ht()
+        circles.remove(b)
   # trtl.update() moves everything at once
   # may produce error on vs code. ignore
   trtl.update()
-  wn.ontimer(animate, 5)
+  wn.ontimer(animate, 60)
 
 # creates new projectile tied to a player
 def new_projectile(player):
@@ -136,7 +193,7 @@ def new_projectile(player):
   circles.append(c)
 
 for k in "wasdilkj":
-    
+  
   # controls movement of both players
   wn.onkeypress(functools.partial(move_player, k),key = k)
   

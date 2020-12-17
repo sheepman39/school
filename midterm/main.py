0c5832d076cf406b09236b3df55f1212092d27b4
sheepman39 <jsalata90@gmail.com>
2020-12-17 20:40:01 +0000

    Commented Code. Finished Project V1.2

--- midterm/main.py
+++ midterm/main.py
@@ -21,12 +21,14 @@ damage = 10
 circles = list()
 font_setup = ("Arial", 20, "bold")
 
+# projectile handling
 player1_projectiles = 0
 player2_projectiles = 0
 max_projectiles = 6
 
-# test turtle
+# Center line
 test1 = trtl.Turtle()
+test1.ht()
 test1.goto(0,100)
 test1.goto(0,-100)
 
@@ -65,7 +67,8 @@ player2_health.penup()
 player2_health.color("green")
 player2_health.goto(150,-150)
 
-
+# update health updates the health and 
+# ammo turtles at the bottom
 def update_health():
 
   player1_health.clear()
@@ -97,7 +100,6 @@ class Projectile():
     self.turtle.setheading(player.heading())
     self.life = 30
 
-
   # move function to calculate which coordinates
   # the projectile goes to
   # Only moves on the x-axis
@@ -108,38 +110,56 @@ class Projectile():
     self.life, dist = self.life - t, speed * t 
     self.x, self.y = self.x + dist * math.cos(self.direction), self.y + dist * math.sin(self.direction)
     
+    # this is all collision checking for player2
     if(abs(self.turtle.xcor()-player2.xcor()) <= 30 and abs(self.turtle.ycor() - player2.ycor()) <= 30 and self.turtle.fillcolor() == player1.fillcolor()):
 
+      # if there is a collection, hide and clear
+      # the turtle
       self.turtle.ht()
       self.turtle.clear()
 
+      # try block removes errors for removing
       try:
+
         circles.remove(self)
+
+        # removes an active projectile from the opposing player
         player1_projectiles -= 1
+        
+        # removes health from player 2
         player2.health -= damage
+
+        # updates the health bar
         update_health()
         return 
+
       except:
+
         return
 
+    # collision checking for player1
     elif(abs(self.turtle.xcor()-player1.xcor()) <= 30 and abs(self.turtle.ycor() - player1.ycor()) <= 30 and self.turtle.fillcolor() == player2.fillcolor()):
 
+      # this is pretty much the same code
+      # as above
       self.turtle.ht()
       self.turtle.clear()
 
       try:
+
         circles.remove(self)
         player2_projectiles -= 1
         player1.health -= damage
         update_health()
         return 
+
       except:
+
         return
 
     else:
+      # if there is no collision, move as normal
       self.turtle.goto(self.x,self.turtle.ycor())      
-
-
 
 # functions
 
@@ -167,16 +187,25 @@ def move_player(key):
     elif(key == "i"):
       player2.goto(player2.xcor(),player2.ycor()+speed)
 
+# life check function check if either player
+# has no health and ends the game
 def life_check():
       
   if(player1.health <= 0):
-
+    
+    # hides the turtles and shows a popup
     player1.ht()
     player2.ht()
     wn.textinput("Game Over","Congrats Player 2 Wins!\nPress cancel to stop playing!")
+    
     try:
+      
+      # if a button is hit, close the game
+      #This causes an error. safe to ignore
       wn.bye()
+    
     except:
+    
       pass
 
   elif(player2.health <= 0):
@@ -184,10 +213,15 @@ def life_check():
     player1.ht()
     player2.ht()
     wn.textinput("Game Over!","Congrats Player 1 Wins!\nPress cancel to stop playing!")
+    
     try:
+    
       wn.bye()
+    
     except:
+    
       pass
+
 # this function animates all projectiles at once
 def animate():
 
@@ -206,11 +240,19 @@ def animate():
 
         # if the life is less than 0, remove it
         if b.life < 0:
+          
           b.turtle.ht()
+          
           try:
+          
             circles.remove(b)
+          
           except:
+          
             pass
+          
+          # if the turtle is from a player and is 
+          # removed, remove one current projectile
           if b.turtle.fillcolor() == player1.fillcolor():
 
             player1_projectiles -= 1
@@ -221,13 +263,16 @@ def animate():
             player2_projectiles -= 1
             update_health
     
-  
-    
   # trtl.update() moves everything at once
   # may produce error on vs code. ignore
   trtl.update()
+
+  # if the player health is low, check it
   if(player1.health < 10 or player2.health < 10):
+    
     life_check()
+  
+  # on timer repeats this function for smoothness
   wn.ontimer(animate, 5)
 
 
@@ -237,6 +282,8 @@ def new_projectile(player):
   global player1_projectiles
   global player2_projectiles
   
+  # checks if the plyaer is allowed to spawn
+  # another Projectile
   if(player.fillcolor() == player1.fillcolor() and player1_projectiles < max_projectiles):
     
     player1_projectiles += 1
@@ -252,6 +299,8 @@ def new_projectile(player):
 
 update_health()
 
+# primary loop that listens for key presses
+# and controls which direction players move in
 for k in "wasdilkj":
   
   # controls movement of both players

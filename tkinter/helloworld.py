ad3a94d7b937092badd8377c79d2646bf9d6b3db
sheepman39 <jsalata90@gmail.com>
2021-01-22 19:34:17 +0000

    Started tkinter tutorial

---
+++ tkinter/helloworld.py
@@ -0,0 +1,19 @@
+import tkinter
+
+# in tkinter, everything is a widget
+# buttons, frames, text boxes ect. are treated as widgets
+# and the main widget is referred as the 'root' widget
+# this line creates that root widget
+root = Tk()
+
+# to create anything, you have to take 2 steps
+# step 1: define the thing
+# step 2: put it on the screen
+
+# we defined what our label is
+# it is going to go in the root widget with text saying 'hello world'
+my_label = Label(root, text = "hello world")
+
+# 2 ways to display stuff
+# for now, only do one
+my_label.pack()
---
+++ tkinter/readme.txt
@@ -0,0 +1,3 @@
+This is going to be me following along through a tutorial on youtube instead of the tutorial provided by in class.
+I will try to document as much as possible
+https://www.youtube.com/watch?v=YXPyB4XeYLA

96a3e2221056884627ce5d11412ddb839ec82a70
sheepman39 <jsalata90@gmail.com>
2021-01-26 15:45:04 +0000

    Continue work

--- Lesson_2.1/2.1.4_window.py
+++ Lesson_2.1/2.1.4_window.py
@@ -4,6 +4,18 @@
 ##############################################################################
 import tkinter as tk
 
+password = None
+# step 21+22
+def test_my_button():
+  global password
+  
+  frame_auth.tkraise()
+
+  # step 26 
+  password = ent_password.get()
+  user_password = tk.Label(frame_auth, text=password)
+  user_password.pack()
+
 # here is a site with some easy-to-use documentation
 # https://www.tutorialspoint.com/python/tk_toplevel.htm
 # for number 7, look at line 13
@@ -14,22 +26,43 @@ root = tk.Tk()
 # step 7
 root.title("Authorization")
 
-root.wm_geometry("200x100")
+root.wm_geometry("200x300")
 
 # step 8 copy and paste
 # create empty frame
 frame_login = tk.Frame(root)
 
-# step 9
-frame_login.grid()
+# new authorization frame
+frame_auth = tk.Frame(root)
+
+# step 9 + 19
+frame_login.grid(row = 0, column = 0, sticky="news")
+
+# step 20
+frame_auth.grid(row = 0, column = 0, sticky = "news")
 
 # step 10
 lbl_username = tk.Label(frame_login, text='Username:',font="Courier",foreground="black")
 lbl_username.pack(padx=50)
 
+# 16 provided code
+ent_username = tk.Entry(frame_login, bd=3)
+ent_username.pack(pady=5)
+
 # steps 11-14ish
 # use foreground="<color>" to change text color
 lbl_password = tk.Label(frame_login,text="Password:",font="Courier",foreground="black")
 lbl_password.pack(pady=25)
+
+# step 17
+ent_password = tk.Entry(frame_login, bd=3, show="*")
+ent_password.pack(pady=5)
+
+# step 18 + 23
+btn_login = tk.Button(frame_login, text="Hack?", command =test_my_button)
+btn_login.pack(pady=25)
+
+# step 21
+frame_login.tkraise()
 
 root.mainloop()

4b860535296d49f15d4c16d10e77eb150d334c96
sheepman39 <jsalata90@gmail.com>
2021-03-05 20:15:49 +0000

    Copy pasted 5

--- Lesson_2.2.7/main.py
+++ Lesson_2.2.7/main.py
@@ -12,8 +12,29 @@ root = tk.Tk()
 frame = tk.Frame(root)
 frame.pack()
 
+# number 5
 # set up button to run the do_command function
 ping_btn = tk.Button(frame, text="ping", command=do_command)
 ping_btn.pack()
+
+# creates the frame with label for the text box
+frame_URL = tk.Frame(root, pady=10,  bg="black") # change frame color
+frame_URL.pack()
+
+# decorative label
+url_label = tk.Label(frame_URL, text="Enter a URL of interest: ", 
+    compound="center",
+    font=("comic sans", 14),
+    bd=0, 
+    relief=tk.FLAT, 
+    cursor="heart",
+    fg="mediumpurple3",
+    bg="black")
+url_label.pack(side=tk.LEFT)
+url_entry= tk.Entry(frame_URL,  font=("comic sans", 14)) # change font
+url_entry.pack(side=tk.LEFT)
+
+frame = tk.Frame(root,  bg="black") # change frame color
+frame.pack()
 
 root.mainloop()

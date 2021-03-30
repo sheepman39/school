9ca2f3f04c4113b079830080e72dc608d5dab5bf
sheepman39 <jsalata90@gmail.com>
2021-03-30 19:24:17 +0000

    Step through 31

--- Lesson_3.2.1/main.py
+++ Lesson_3.2.1/main.py
@@ -25,3 +25,35 @@ plt.title('Change in Temperatures')
 plt.show()
 
 # TODO #5: Calculate min, max, and avg anomaly and the corresponding min/max years
+min_anomaly = temp_data['Anomaly'][0]
+max_anomaly = temp_data['Anomaly'][0]
+min_year = temp_data['Year'][0]
+max_year = temp_data['Year'][0]
+sum_anomaly = 0
+avg_anomaly = 0
+
+# find the min, max and calculate the sum
+for i in range(0, len(temp_data['Anomaly'])):
+  if (temp_data['Anomaly'][i] < min_anomaly):
+    min_anomaly = temp_data['Anomaly'][i]
+    min_year = temp_data['Year'][i]
+  
+  # extra for loop for step 25
+  if (temp_data['Anomaly'][i] > max_anomaly):
+    max_anomaly = temp_data['Anomaly'][i]
+    max_year = temp_data['Year'][i]
+  
+  # step 26
+  # new code inside the same for loop
+  sum_anomaly = sum_anomaly + temp_data['Anomaly'][i]
+
+  # step 27
+  # calculate average
+  avg_anomaly = sum_anomaly/len(temp_data['Anomaly'])
+
+# step 28
+# note this is outside of the for loop
+# print the statistical values
+print("The maximum anomaly is:", max_anomaly, "which occured in", max_year)
+print("The minimum anomaly is:", min_anomaly, "which occured in", min_year)
+print("The average anomaly is:", avg_anomaly)

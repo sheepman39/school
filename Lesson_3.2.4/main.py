1ee4b2a57a8e8c3acec9fc658d5bc2d087a88d85
sheepman39 <jsalata90@gmail.com>
2021-04-22 19:46:57 +0000

    Fixed 18

--- Lesson_3.2.4/main.py
+++ Lesson_3.2.4/main.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-df = pd.read_csv("./Lesson_3.2.4/honey_1997.csv", header=0)
+df = pd.read_csv("./Lesson_3.2.4/honey_1977.csv", header=0)
 
 df['Value'] = df['Value'].str.replace(',', '')
 df['Value'] = pd.to_numeric(df['Value'], errors='coerce')
@@ -17,17 +17,20 @@ years = honey_sums.keys()
 all_honey = []
 all_states = []
 
-''' # without grouping
-for state in unique_states:
-  honey_data = df[df['State'] == state]['Value']
-  print (state, honey_data.sum())
-  all_honey.append(honey_data.sum())
-  all_states.append(state)
-'''  
 # with grouping
 for state in unique_states:
   honey_data = df[df['State'] == state].groupby('Year')['Value']
-  print (state, honey_data.sum())
   all_honey.append(honey_data.sum())
   all_states.append(state)
-  
+
+for i in range(len(all_honey)):
+  honey = all_honey[i]
+  state = all_states[i]
+  years = honey.keys()
+  plt.plot(years, honey, label=state)
+
+plt.ylabel('Honey Production')
+plt.xlabel('Year')
+plt.title('Honey Production')
+plt.legend()
+plt.show()

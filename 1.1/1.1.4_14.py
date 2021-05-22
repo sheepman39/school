b60fa7790d4044d633ae287abb174ffe2bbecf0d
sheepman39 <jsalata90@gmail.com>
2021-05-22 09:39:08 -0500

    Starting to write notes

--- 1.1/1.1.4_14.py
+++ 1.1/1.1.4_14.py
@@ -1,13 +1,18 @@
 #   a114_nested_loops_2.py 
 import turtle as trtl
 
+# setup of the turtle
 painter = trtl.Turtle()
 painter.shape("circle")
 painter.hideturtle()
 painter.penup()
 
+# sets the default x position to -200
 x = -200
+
 while (x < 200): 
+
+  # since this loop will continue
   x = x + 50
   y = 200
   while (y > -200):

abe48b5c4494baf4cfb3567a000f70948f3cb1aa
sheepman39 <jsalata90@gmail.com>
2021-05-28 10:09:17 -0500

    Added comments

--- 1.1/1.1.4_14.py
+++ 1.1/1.1.4_14.py
@@ -13,17 +13,34 @@ x = -200
 while (x < 200): 
 
   # since this loop will continue
+  # x will be increased by 50 each time 
+  # y will be reset each time the loop runs to 200
   x = x + 50
   y = 200
+  
   while (y > -200):
+    
+    # to control movement, y is subtracted by 50 each time
+    # the loop will keep running as long as y is greater than -200
     y -= 50
+    
+    # the turtle will go to coordinates x,y and set the color to orange
     painter.goto(x,y)
     painter.color("orange")
+    
+    # if the x position is -150, it will set the color to purple instead
     if(x == -150):
+      
       painter.color("purple")
+    
+    # then the turtle will stamp its shape in the same color as determined above
     painter.stamp()
+    
+    
 #This Program was created/edited by sheepman39
 #In case of plagarism, please go to https://github.com/sheepman39/school
 #And submit an issue
+
+# creates and displays the screen
 wn = trtl.Screen()
 wn.mainloop()

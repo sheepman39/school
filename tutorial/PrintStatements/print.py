b6c35ad921dacf3124b7fc3e88d4739e135e53c5
sheepman39 <jsalata90@gmail.com>
2021-08-05 11:24:16 -0500

    Started print statements

---
+++ tutorial/PrintStatements/print.py
@@ -0,0 +1,31 @@
+# Comments in code are great to write down notes to remember what is going on
+# In python, put a # in front of a line to make a comment and the computer will ignore it
+'''
+Using three ' can help make multi-line comments for longer sections
+The computer will also ignore everything in these lines
+'''
+
+# In python, text is known as a String
+# In order to print out a string, use the print command
+print("Hello World!")
+
+# Print("Hello World!") will NOT work because print is capitalized
+# print(hello world!) will also not work because the message is not in quotation marks
+print('Hello World!') # will work with single apostrophes.  As long as the quotation marks/apostrophes match it will work
+
+# Using a backslash \ will tell python to print it literally and not count it as a mark
+# In the following line, without the \ in front of " this line would not work
+print("A wise man once told me \"Always remember to eat your vegetables\"")
+print('I can\'t do that')
+
+# TODO: Print out a 5 line story using 5 different print commands, one of which with quotation marks or apostrophes
+
+
+
+
+
+# In addition to being useful in quotes, the backslash can also provide different utilities
+# \n will start a new line
+# \t will create a tab
+print("First line \nSecond Line")
+print("I wish I was tabbed \t Tabs!")

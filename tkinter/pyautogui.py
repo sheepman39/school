14de28029d4444e0f4f13262c227913eec81cacd
sheepman39 <51085651+sheepman39@users.noreply.github.com>
2021-10-11 12:35:33 -0500

    Update pyautogui.py

--- tkinter/pyautogui.py
+++ tkinter/pyautogui.py
@@ -1,16 +1,19 @@
 import pyautogui
 import time
 
+
 pyautogui.FAILSAFE = True
 
-for i in range(0,10):
+
+while True:
     
+    time.sleep(20)
     print("Moving to the right")
     pyautogui.moveRel(300,0,3, pyautogui.easeInBounce)
     time.sleep(20)
     pyautogui.click()
     time.sleep(15)
-    print("Moving down")
+    print("Moving up")
     pyautogui.moveRel(0,-300,3, pyautogui.easeInElastic)
     time.sleep(20)
     pyautogui.click()
@@ -20,6 +23,5 @@ for i in range(0,10):
     time.sleep(20)
     pyautogui.click()
     time.sleep(15)
-    print("Moving up")
+    print("Moving down")
     pyautogui.moveRel(0,300,3, pyautogui.easeInElastic)
-    

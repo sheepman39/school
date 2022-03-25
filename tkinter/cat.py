f2d5a715f7282ef40da09db58b8682fa27b04240
sheepman39 <51085651+sheepman39@users.noreply.github.com>
2022-03-25 09:08:12 -0500

    Update cat.py

--- tkinter/cat.py
+++ tkinter/cat.py
@@ -1,18 +1,25 @@
 import requests
+from PIL import Image
+
+# file to save image to
+FILE = "dog.jpg"
 
 # opens up a file to store the cat image in
 # 'wb' stands for write in binary, telling the computer to save the image as a binary
-image = open("cat.gif","wb")
+image = open(FILE,"wb")
 
 # the API we will be using is Cat As A Service
+URL = "https://place.dog/1920/1080"
 URL = "https://cataas.com/"
 
 # gets a gif from the API
-response = requests.get(URL+"/c/gif")
+response = requests.get(URL+"/c")
 
 # if the response is good, save the image
 if response.status_code == 200:
     image.write(response.content)
+    im = Image.open(FILE)
+    im.show()
 
 # if not, print out an error
 else:
